60634b60d8b763860c504616ecd566ef68d3d63d
Tanuki <97910292+TanukiTsohiko@users.noreply.github.com>
2022-01-19 18:19:07 +0900

    add decals limit patch x)

--- patterns/Hooking.h
+++ patterns/Hooking.h
@@ -34,13 +34,23 @@ namespace hook
 	template<typename ValueType, typename AddressType>
 	inline void put(AddressType address, ValueType value)
 	{
+		DWORD oldProtect;
+		VirtualProtect((void*)address, sizeof(value), PAGE_EXECUTE_READWRITE, &oldProtect);
+
 		memcpy((void*)address, &value, sizeof(value));
+
+		VirtualProtect((void*)address, sizeof(value), oldProtect, &oldProtect);
 	}
 
 	template<typename AddressType>
 	inline void nop(AddressType address, size_t length)
 	{
+		DWORD oldProtect;
+		VirtualProtect((void*)address, length, PAGE_EXECUTE_READWRITE, &oldProtect);
+
 		memset((void*)address, 0x90, length);
+
+		VirtualProtect((void*)address, length, oldProtect, &oldProtect);
 	}
 
 	void* AllocateFunctionStub(void* origin, void* function, int type);
